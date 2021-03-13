a8409c1b222975d95ab199daba54c83f37be6d51
NePPPlus_010 <kmc901206@gmail.com>
2021-03-13 16:16:44 +0900

    ArrayAdapter가 동작하기 위해 필요한 재료를 받아오고 => 넘겨주기 까지

--- app/src/main/java/com/example/listviewpractice_20210313/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_20210313/adapters/StudentAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.listviewpractice_20210313.adapters
 
-// ArrayAdapter를 상속받음
+// ArrayAdapter를 상속받음 ( : ArrayAdapter)
+import android.content.Context
 import android.widget.ArrayAdapter
-// Student를 받아서 뿌릴꺼야
+// Student를 받아서 뿌릴꺼야 (<Student = 방금 만든 데이터 class>()
 import com.example.listviewpractice_20210313.datas.Student
-
-class StudentAdapter : ArrayAdapter<Student>() {
+// 3가지 받아오고 받아오는 순서대로 대입
+class StudentAdapter(
+    val mContext : Context, 
+    val resId : Int, 
+    val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
 }
