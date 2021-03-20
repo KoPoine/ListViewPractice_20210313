eacddacd99e3b79fa801d432c6a52d7982e6782b
NePPPlus_010 <kmc901206@gmail.com>
2021-03-20 12:47:48 +0900

    함수 추가 예제

--- app/src/main/java/com/example/listviewpractice_20210313/datas/Student.kt
+++ app/src/main/java/com/example/listviewpractice_20210313/datas/Student.kt
@@ -1,8 +1,31 @@
 package com.example.listviewpractice_20210313.datas
 
+import java.util.*
+
 // 학생 클래스가 가져야 할 변수 val로 표현, 길어지면 가독성 떨어지니 val 별로 줄을 띄어서 표시
 class Student(
     val name : String,
     val birthYear : Int,
     val address : String) {
+
+//    학생 클래스가 수행할 수 있는 기능 - 함수 추가
+
+//    나의 현재 나이를 계산해서 결과로 주는 함수
+
+    fun getKoreanAge() : Int {
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+
+        val koreanAge = currentYear - this.birthYear + 1
+
+        return koreanAge
+    }
+
+    fun addTwoNumbers(num1 : Int, num2 : Int) : Int {
+
+        val result = num1 + num2
+
+        return result
+
+    }
+
 }
