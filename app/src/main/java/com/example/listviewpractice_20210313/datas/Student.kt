db49cf19c42f515435b313c8398fb15d8d804430
NePPPlus_010 <kmc901206@gmail.com>
2021-03-20 11:35:57 +0900

    거주기 추가에 필요한 코드들만 수정

--- app/src/main/java/com/example/listviewpractice_20210313/datas/Student.kt
+++ app/src/main/java/com/example/listviewpractice_20210313/datas/Student.kt
@@ -3,5 +3,6 @@ package com.example.listviewpractice_20210313.datas
 // 학생 클래스가 가져야 할 변수 val로 표현, 길어지면 가독성 떨어지니 val 별로 줄을 띄어서 표시
 class Student(
     val name : String,
-    val birthYear : Int) {
+    val birthYear : Int,
+    val address : String) {
 }
