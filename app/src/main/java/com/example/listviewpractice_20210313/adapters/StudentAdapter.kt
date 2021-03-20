362167f33a2ba89bda9a40d27561c1195b50c379
NePPPlus_010 <kmc901206@gmail.com>
2021-03-20 10:16:50 +0900

    화면에 뿌려줄 xml을 그려야하는 상황에만 그리도록

--- app/src/main/java/com/example/listviewpractice_20210313/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_20210313/adapters/StudentAdapter.kt
@@ -3,7 +3,10 @@ package com.example.listviewpractice_20210313.adapters
 // ArrayAdapter를 상속받음 ( : ArrayAdapter)
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listviewpractice_20210313.R
 // Student를 받아서 뿌릴꺼야 (<Student = 방금 만든 데이터 class>()
 import com.example.listviewpractice_20210313.datas.Student
 // 3가지 받아오고 받아오는 순서대로 대입
@@ -15,5 +18,22 @@ class StudentAdapter(
 //    xml > 코틀린 변수로 꺼내오도록 도와주는 클래스. (LayoutInflater)
     val inflater = LayoutInflater.from(mContext)
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+//        return 지우는 행위 >> 기존의 결과를 버리고 새로우 결과를 직접 만들어서 사용
+//        position : 지금 그려주려는 위치가 어디인지를 알려준다.
+//        convertView : 재활용 할 요소가 있는지 확인하고 있다면 활용, 없다면 새로 그려주자.
+
+        var tempRow = convertView
+
+//        재활용 할 요소가 없나? => 없으면 그려줘야함
+        if (tempRow == null) {
+//            if 문 내부로 진입 > tempRow 내부가 비어있는 상황! => 채워주자
+            tempRow = inflater.inflate(R.layout.student_list_item, null)
+        }
+
+
+
+    }
 
 }
